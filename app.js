b723276e2f6aaaded407125134688e617c046b86
Teddy <t.boirin@icloud.com>
2020-03-17 01:29:00 +0100

    etap 17

--- app.js
+++ app.js
@@ -5,17 +5,33 @@ const axios = require('axios');
 const app = express();
 app.set('view engine', 'pug');
 app.use(bodyParser.urlencoded({ extended: true }));
+
+
 app.get('/', function(req, res) {
     res.render('index', { title: 'Qui prend quoi ?' });
 })
 
+
 app.post('/party', function(req, res) {
-    res.send('Post ok !')
+    res.send('Post ok !');
     axios
         .post(`${process.env.API_URL}/party`, req.body)
-        .then(({data}) => console.log(data))
-        .catch((err) => console.error(err));
+        .then(({ data }) => res.redirect(`/party/${data._id}`))
+        .catch((err) => res.send(err));
 })
 
+app.get('/party/:id', function(req, res) {
+    axios
+        .get(`${process.env.API_URL}/party/${req.params.id}`)
+        .then(({ data }) =>
+            res.render('party', {
+                party: data,
+                title: data.name,
+                url: `${process.env.FRONT_URL}:${process.env.PORT}/party/${data._id}`
+            }),
+        )
+        .catch((err) => console.log(err));
+
+})
 
 app.listen(process.env.PORT, () => console.log(`Front app listening on port ${process.env.PORT}!`));
