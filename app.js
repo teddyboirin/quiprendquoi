bf5d8773ec4e8a26f526dd71ff613e3aad2ba10d
Teddy <t.boirin@icloud.com>
2020-03-17 01:07:29 +0100

    solve problem

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 const express = require('express');
-const app = express();
-const bodyParser = require('body-parser');
 const dotenv = require('dotenv').config();
-const port = process.env.PORT;
+const bodyParser = require('body-parser');
 const axios = require('axios');
-app.use(bodyParser.urlencoded({ extended: true }));
+const app = express();
 app.set('view engine', 'pug');
+app.use(bodyParser.urlencoded({ extended: true }));
 app.get('/', function(req, res) {
     res.render('index', { title: 'Qui prend quoi ?' });
 })
 
 app.post('/party', function(req, res) {
+    res.send('Post ok !')
     axios
         .post(`${process.env.API_URL}/party`, req.body)
         .then(({data}) => console.log(data))
@@ -18,4 +18,4 @@ app.post('/party', function(req, res) {
 })
 
 
-app.listen(port, () => console.log(`Front app listening on port ${port}!`));
+app.listen(process.env.PORT, () => console.log(`Front app listening on port ${process.env.PORT}!`));
