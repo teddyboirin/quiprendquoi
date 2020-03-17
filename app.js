e21fcb03fdf5f2bbb56a6acd83da5c7b29a5ad13
Teddy <t.boirin@icloud.com>
2020-03-17 15:34:35 +0100

    etap 22

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const axios = require('axios');
 const app = express();
 app.set('view engine', 'pug');
 app.use(bodyParser.urlencoded({ extended: true }));
-
+app.use(express.static('public'));
 
 app.get('/', function(req, res) {
     res.render('index', { title: 'Qui prend quoi ?' });
